8875d9c92f25fc310cb030b18c446e5cb446c520
lenilunderman <lenilunderman@gmail.com>
2022-01-03 00:18:42 -0500

    charCount options

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function charCount(str){
     // make the object
     const result = {}
     // loop the object
+    // for(let char of str) {
+    //    char = str.toLowerCase(); // return the first character
+
     for(let i = 0; i < str.length; i++) {
         const char = str[i].toLowerCase(); // return the first character
         // add the number of character in the property
